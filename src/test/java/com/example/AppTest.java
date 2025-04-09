7392aefbd105454446736d29a200d1250a236a28
podilithanoj <psn.thanoj@gmail.com>
2025-04-09 08:26:31 +0530

    added

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertEquals(2, 1 + 1);
+    }
+}
